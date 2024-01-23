bcb97dfe1bf3241ef41020dc156569047626475a
Sappanarasimhamurty <sappanarasimhamurty@gmail.com>
2024-01-23 14:22:44 +0530

    Username Example added

---
+++ 05-Variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Please Enter Your User Name"
+read USERNAME # This is the variable where the value of the one user entered
+
+echo "UserName is Entered:$USERNAME"
+
+
+
+
+
+
+

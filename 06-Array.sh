f0f23c2e0e86027304ab5939f592b15fd2f4e8ed
Sappanarasimhamurty <sappanarasimhamurty@gmail.com>
2024-01-23 15:06:32 +0530

    Array  Example added

--- 06-Array.sh
+++ 06-Array.sh
@@ -5,3 +5,6 @@ PERSONS=("Viswa" "Rajesh" "Narasimha")
 #Inside array:we always refer a value index starting from 0
 
 echo "First person:${PERSONS[0]}"
+
+#@Means all persons
+echo "First person:${PERSONS[@]}"

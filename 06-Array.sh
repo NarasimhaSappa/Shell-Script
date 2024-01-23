e8556edd740c6f2e96877a248fe65e24bc5ab619
Sappanarasimhamurty <sappanarasimhamurty@gmail.com>
2024-01-23 15:02:23 +0530

    Array  Example added

---
+++ 06-Array.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#This is called array ,Instead of single value it can be multiple values
+PERSONS=("Viswa" "Rajesh" "Narasimha")
+#Inside array:we always refer a value index starting from 0
+
+echo "First person:${PERSONS[0]}"

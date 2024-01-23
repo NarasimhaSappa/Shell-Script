b8132bb678ce8f313ce30af0602c7e2de86aa179
Sappanarasimhamurty <sappanarasimhamurty@gmail.com>
2024-01-23 08:27:40 +0530

    Hello Shell script added

--- 01-Hello-world.sh
+++ 01-Hello-world.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 echo "Hello World.I am learning Devops with AWS"
+echo "Ramesh: Hi suresh ,Good Morning"
+echo "Suresh: Good Morning Ramesh"
+echo "Ramesh: How are you Suresh"
+echo "Ramesh: I am good,How are you Murthy"
